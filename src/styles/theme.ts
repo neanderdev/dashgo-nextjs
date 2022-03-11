a6e08710fcd0d43ed34e01cde17c7c487cd303ad
neanderdev <neander.faria@hotmail.com>
2022-03-11 16:45:24 -0300

    Creating user list page

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,13 @@
 import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
+    components: {
+        Button: {
+            baseStyle: {
+                cursor: 'pointer',
+            },
+        },
+    },
     colors: {
         gray: {
             "900": "#181B23",
