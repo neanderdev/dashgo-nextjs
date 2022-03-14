9dfe7f4469d8e4acabb2417143c2b7dab12b1239
neanderdev <neander.faria@hotmail.com>
2022-03-14 10:51:52 -0300

    Responsive application dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -76,7 +76,7 @@ export default function Dashboard() {
 
                 <SimpleGrid flex='1' gap='4' minChildWidth='320px' alignItems='flex-start'>
                     <Box
-                        p='8'
+                        p={['6', '8']}
                         bg='gray.800'
                         borderRadius={8}
                         pb='4'
@@ -91,7 +91,7 @@ export default function Dashboard() {
                     </Box>
 
                     <Box
-                        p='8'
+                        p={['6', '8']}
                         bg='gray.800'
                         borderRadius={8}
                         pb='4'
