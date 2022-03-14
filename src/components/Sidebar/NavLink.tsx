c5b505a15501082292b4e6167e959bd3cb709a6a
neanderdev <neander.faria@hotmail.com>
2022-03-14 13:32:51 -0300

    Flagging the active link in the sidebar

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,11 +1,12 @@
 import { ElementType } from 'react';
-import Link from 'next/link';
 import {
   Icon,
   Text,
   Link as ChakraLink,
   LinkProps as ChakraLinkProps,
 } from '@chakra-ui/react';
+
+import { ActiveLink } from '../ActiveLink';
 
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType;
@@ -15,13 +16,13 @@ interface NavLinkProps extends ChakraLinkProps {
 
 export function NavLink({ icon, titleNavLink, href, ...rest }: NavLinkProps) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" alignItems="center" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">
           {titleNavLink}
         </Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   );
 }
