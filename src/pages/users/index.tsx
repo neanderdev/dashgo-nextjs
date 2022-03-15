15024d88afcdb9dc47b3ae953056b25d69b7dd0d
neanderdev <neander.faria@hotmail.com>
2022-03-15 09:18:28 -0300

    Listing user with React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,15 +12,26 @@ export default function UserList() {
         const response = await fetch('http://localhost:3000/api/users');
         const data = await response.json();
 
-        return data;
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric',
+                }),
+            };
+        });
+
+        return users;
     });
 
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
     });
-
-
 
     return (
         <Box>
@@ -76,83 +87,35 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    <Tr>
-                                        <Td px={['4', '4', '6']}>
-                                            <Checkbox colorScheme='pink' />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight='bold'>Neander de Souza</Text>
-                                                <Text fontSize='sm' color='gray.300'>mcspipoca47@gmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>11 de Março de 2022</Td>}
-                                        {isWideVersion && (
-                                            <Td>
-                                                <Button
-                                                    as='a'
-                                                    size='sm'
-                                                    fontSize='sm'
-                                                    colorScheme='purple'
-                                                    leftIcon={<Icon as={RiEditLine} fontSize='16' />}
-                                                >
-                                                    Editar
-                                                </Button>
-                                            </Td>
-                                        )}
-                                    </Tr>
-
-                                    <Tr>
-                                        <Td px={['4', '4', '6']}>
-                                            <Checkbox colorScheme='pink' />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight='bold'>Neander de Souza</Text>
-                                                <Text fontSize='sm' color='gray.300'>mcspipoca47@gmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>11 de Março de 2022</Td>}
-                                        {isWideVersion && (
-                                            <Td>
-                                                <Button
-                                                    as='a'
-                                                    size='sm'
-                                                    fontSize='sm'
-                                                    colorScheme='purple'
-                                                    leftIcon={<Icon as={RiEditLine} fontSize='16' />}
-                                                >
-                                                    Editar
-                                                </Button>
-                                            </Td>
-                                        )}
-                                    </Tr>
-
-                                    <Tr>
-                                        <Td px={['4', '4', '6']}>
-                                            <Checkbox colorScheme='pink' />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight='bold'>Neander de Souza</Text>
-                                                <Text fontSize='sm' color='gray.300'>mcspipoca47@gmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>11 de Março de 2022</Td>}
-                                        {isWideVersion && (
-                                            <Td>
-                                                <Button
-                                                    as='a'
-                                                    size='sm'
-                                                    fontSize='sm'
-                                                    colorScheme='purple'
-                                                    leftIcon={<Icon as={RiEditLine} fontSize='16' />}
-                                                >
-                                                    Editar
-                                                </Button>
-                                            </Td>
-                                        )}
-                                    </Tr>
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={['4', '4', '6']}>
+                                                    <Checkbox colorScheme='pink' />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight='bold'>{user.name}</Text>
+                                                        <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>{user.createdAt}</Td>}
+                                                {isWideVersion && (
+                                                    <Td>
+                                                        <Button
+                                                            as='a'
+                                                            size='sm'
+                                                            fontSize='sm'
+                                                            colorScheme='purple'
+                                                            leftIcon={<Icon as={RiEditLine} fontSize='16' />}
+                                                        >
+                                                            Editar
+                                                        </Button>
+                                                    </Td>
+                                                )}
+                                            </Tr>
+                                        );
+                                    })}
                                 </Tbody>
                             </Table >
 
